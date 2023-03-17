9252e2cc9ca03ba5c568b5d291e8979aa386e3d5
JoaoGracio <joao.gracio@noop.pt>
2023-03-17 09:50:15 +0000

    MarketingSMSChannel being sorted out;

--- src/MarketingSMSChannel.php
+++ src/MarketingSMSChannel.php
@@ -15,11 +15,38 @@ class MarketingSMSChannel
         $this->client = $client;
     }
 
-    public function send($notifiable, Notification $notification)
+    public function send($notifiable, Notification $notification): void
     {
+        $phones = $notifiable->routeNotificationFor('marketingSMS');
+
+        if (!$phones) {
+            $phones = $notifiable->routeNotificationFor(MarketingSMSChannel::class);
+        }
+
+        if (!$phones) {
+            return;
+        }
+
         $message = $notification->toMarketingSMS($notifiable);
+
+        if(is_string($message)){
+            $message = new MarketingSMSMessage($message);
+        }
+
+        if(! $message instanceof MarketingSMSMessage){
+            return;
+        }
+
+        $response = $this->client->sendMessage([
+            'name' => $message->name,
+            'message' => $message->message,
+            'from' => $message->from ?? config('marketing_sms.from'),
+            'delivered_at' => $message->deliveredAt ?? null,
+            'phones' => $message->phones,
+        ]);
+
         $response = $this->client->sendMessage($message);
-        if ($response->getStatusCode() != 200) {
+        if ($response->getResponseCode() != 200) {
             throw Exceptions\CloudNotSendNotification::marketingSMSError($response->getCleanResponse(), $response->getResponseCode());
         }
     }
